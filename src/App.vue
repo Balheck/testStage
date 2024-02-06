cdd062dfe2025b76d47a61302cf6131d6f48747a
Balheck <landadamien@gmail.com>
2024-02-06 13:19:22 +0100

    Envoie du projet test stage

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!-- App.vue -->
+<template>
+  <test2></test2>
+  <test3></test3>
+
+  <FormSelect v-model="selectedOption" theme="dark">
+    <option class="bg-white m-0" value="">Toutes les activitiés</option>
+    <option class="bg-white m-0">1</option>
+    <option class="bg-white m-0">2</option>
+    <option class="bg-white m-0">3</option>
+    <option class="bg-white m-0">4</option>
+  </FormSelect>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import FormSelect from './components/test.vue';
+const selectedOption = ref('');
+
+import test2 from './components/test2.vue';
+</script>
---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<!-- components/FormSelect.vue -->
+<template>
+    <div 
+      class="flex items-center justify-between gap-5 px-4 text-sm relative"
+      :class="{
+            'bg-gray-100 py-2 rounded-md': theme == 'gray',
+            'bg-gray-900 text-black rounded-xl py-2': theme == 'dark',
+            'bg-primary text-black m-md-2 rounded-xl py-2': theme == 'primary'
+      }"
+    >
+        <select
+          v-model="filters"
+          class="bg-transparent m-0 p-0 appearance-none w-full pr-5"
+        >
+            <slot />
+        </select>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+  
+const { theme } = defineProps(['theme']);
+</script>
+  
